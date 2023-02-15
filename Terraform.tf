e8c1ca9fe6f4496dac33af25bab49c4a3ebd6188
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2023-02-15 14:43:40 +0530

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -2,20 +2,20 @@ provider "aws" {
   region ="Chose your region accoding to your reqirment"
   access_key="yor_aws_access_key"
   secret_key="your_aws_secret_key"
-  //these two key in your profile section user Security credentials
+#   these two key in your profile section user Security credentials
 }
 
 resource "aws_instance" "testcase1"{
   ami="ami-0557a15b87f6559cf"
-  //this ami will change time to time by aws so you can choose any ami acoding to you reqirment.
+#  this ami will change time to time by aws so you can choose any ami acoding to you reqirment.
   instance_type="t2.micro"
   key_name="key_pair_name"
-  //if you dont use key_name here aws will defultly add key_pair 
+#  if you dont use key_name here aws will defultly add key_pair 
   vpc_security_group_ids=["sg-***************"]
-  // //if you dont use security_group here aws will defultly add security group to this ec2-instance 
+#  if you dont use security_group here aws will defultly add security group to this ec2-instance 
 }
 
-//these two resource is optional one becuase these two will be used to stop  instance to run and running instance to stop using terraform
+# these two resource is optional one becuase these two will be used to stop  instance to run and running instance to stop using terraform
 resource "aws_ec2_instance_state" "testcase1"{
   instance_id= aws_instance.testcase1.id
   state="running"
