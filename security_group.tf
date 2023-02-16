059d138aaeeaab8d4852e9aa92d2f485620f7f2d
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2023-02-16 15:28:52 +0530

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -1,19 +1,21 @@
 resource "aws_security_group" "demo-sg" {
   name = “sec-group”
   description = "Allow HTTPS, HTTP and SSH traffic via Terraform"
-
+# HTTP traffic
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  # SSH traffic
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+   # HTTPS traffic
   ingress {
     from_port   = 443
     to_port     = 443
