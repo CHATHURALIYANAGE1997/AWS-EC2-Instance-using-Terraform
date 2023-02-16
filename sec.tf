451ccaccf278d67fee596a265ca38a4c74ce696d
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2023-02-16 15:25:47 +0530

    Update sec.tf

--- sec.tf
+++ sec.tf
@@ -1,1 +1,13 @@
+resource "aws_security_group" "demo-sg" {
+  name = “sec-group”
+  description = "Allow HTTPS, HTTP and SSH traffic via Terraform"
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  
+}
